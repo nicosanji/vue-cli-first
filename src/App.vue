bdba26cd3d9a8a4db9f2f1a0cb6128ac7cbb5c33
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-11-10 23:19:55 +0100

    DEF. 1.1: spostato <Title> in alto

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
+    <Title></Title>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <Title></Title>
   </div>
 </template>
 
